3b290fa375fbb3e3b175f2322950a573235a5f1a
Arnyfavy <arnoldokeyehieze@gmail.com>
2022-06-30 16:42:15 -0700

    fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - prints the phrase with proper grammer,
  * but the outcome is a piece of art,
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
