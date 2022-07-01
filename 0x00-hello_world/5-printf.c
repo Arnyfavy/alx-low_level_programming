2aabdca9e270a0b0c6514218041b5b590091fafa
Arnyfavy <arnoldokeyehieze@gmail.com>
2022-07-01 05:47:57 -0700

    Fifth commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the phrase with proper grammer,
+ * main - Write a C program that prints exactly with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
