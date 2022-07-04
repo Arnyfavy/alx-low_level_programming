c3c2c1035bab0c67806c9e587c7a49402f1de30e
Arnyfavy <arnoldokeyehieze@gmail.com>
2022-07-04 06:48:25 -0700

    sixth commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-putchar((num % 10) + '0');
-putchar('n');
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d\n", n);
+}
+
 return (0);
 }
