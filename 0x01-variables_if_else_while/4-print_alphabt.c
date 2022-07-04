5b4ab79fb0059e067d29fdc3c5195c34233d4eba
Arnyfavy <arnoldokeyehieze@gmail.com>
2022-07-04 06:25:15 -0700

    fifth commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,16 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num < 10; num++)
-printf("%d", num);
-printf("\n");
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+
+putchar(10); /** this is an ascii code for new line*/
+
 return (0);
 }
