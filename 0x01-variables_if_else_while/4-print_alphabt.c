2dba344eb7489e1fa238ebb9a6c27188e22d8416
Arnyfavy <arnoldokeyehieze@gmail.com>
2022-07-04 05:47:58 -0700

    fifth commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the alphabet in lowercase,
+ * followed by a new line.
+ * Return: Always 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
